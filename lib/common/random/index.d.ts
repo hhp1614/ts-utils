1cbc5aefcf55027143bc88b272c29259d3e35ae0
hhp1614 <hhp1614@qq.com>
2020-06-30 12:15:03 +0800

    feat(common): 增加uuid方法

--- lib/common/random/index.d.ts
+++ lib/common/random/index.d.ts
@@ -1,3 +1,4 @@
 export * from './randomNum';
 export * from './randomColor';
+export * from './uuid';
 //# sourceMappingURL=index.d.ts.map
---
+++ lib/common/random/uuid.d.ts
@@ -0,0 +1,5 @@
+/**
+ * 生成 UUID
+ */
+export declare function uuid(): string;
+//# sourceMappingURL=uuid.d.ts.map
